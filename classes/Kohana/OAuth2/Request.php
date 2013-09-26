f7a1709217815b4a53ee2f4c9a6f061b7dc44126
Ilya Gudkov <pridemon@yandex.ru>
2013-09-26 16:54:18 +0700

    add ucfirst to factory methods

--- classes/Kohana/OAuth2/Request.php
+++ classes/Kohana/OAuth2/Request.php
@@ -21,7 +21,7 @@ abstract class Kohana_OAuth2_Request extends OAuth_Request {
 	 */
 	public static function factory($type, $method, $url = NULL, array $params = NULL)
 	{
-		$class = 'OAuth2_Request_'.$type;
+		$class = 'OAuth2_Request_'.ucfirst($type);
 
 		return new $class($method, $url, $params);
 	}
