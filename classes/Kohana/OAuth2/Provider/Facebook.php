d50e5d1f09045261ac977a41b8cb00999cea921f
featherman <featherman101@gmail.com>
2017-03-07 11:41:45 +0700

    switch facebook version to 2.8

--- classes/Kohana/OAuth2/Provider/Facebook.php
+++ classes/Kohana/OAuth2/Provider/Facebook.php
@@ -6,12 +6,12 @@ abstract class Kohana_OAuth2_Provider_Facebook extends OAuth2_Provider {
 
 	public function url_authorize()
 	{
-		return 'https://www.facebook.com/v2.3/dialog/oauth';
+		return 'https://www.facebook.com/v2.8/dialog/oauth';
 	}
 
 	public function url_access_token()
 	{
-		return 'https://graph.facebook.com/v2.3/oauth/access_token';
+		return 'https://graph.facebook.com/v2.8/oauth/access_token';
 	}
 
 
